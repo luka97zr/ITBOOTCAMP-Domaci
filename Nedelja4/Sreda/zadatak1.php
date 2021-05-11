e851e894f276e40e34645328b7ea40c747b9404d
luka97zr <luka97zr@gmail.com>
2021-05-11 19:34:56 +0200

    Nedelja 4

--- Nedelja4/Sreda/zadatak1.php
+++ Nedelja4/Sreda/zadatak1.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 $a = 200;
@@ -24,3 +33,5 @@ $zbir = $a + $b + $c;
 echo "Zbir brojeva $a, $b i $c je $zbir";
 
 ?>
+</body>
+</html>
