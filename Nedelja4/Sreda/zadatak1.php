a88c5e0a659ee599e7c0370fd4418e62a83feb58
luka97zr <luka97zr@gmail.com>
2021-05-11 19:01:51 +0200

    Nedelja4

---
+++ Nedelja4/Sreda/zadatak1.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = 200;
+$b = 80;
+
+$zbir = $a + $b;
+echo $zbir."<br/>";
+
+$proizvod = $a * $b;
+echo $proizvod."<br/>";
+
+$razlika = $a - $b;
+echo $razlika."<br/>";
+
+$kolicnik = $a / $b;
+echo $kolicnik."<br/>";
+
+$a = 7;
+$b = 2;
+$c = 9;
+
+$zbir = $a + $b + $c;
+
+echo "Zbir brojeva $a, $b i $c je $zbir";
+
+?>
