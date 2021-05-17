c768334e8856bf2a51faa1dec581c523daec25fa
luka97zr <luka97zr@gmail.com>
2021-05-17 18:18:51 +0200

    Domaci

--- Nedelja4/Nedelja/zadatak13D_2.php
+++ Nedelja4/Nedelja/zadatak13D_2.php
@@ -12,27 +12,25 @@
      $b = mt_rand(1,20);
      $c = mt_rand(1,20);
 
-     echo "Brojevi:$a,$b,$c<br>";
+     echo "<p>Brojevi:$a,$b,$c</p>";
      
-    //Trebalo bi da radi sve dok ne dodje do preklapanja istih brojeva sto ne znam kako da resim
-    if($a<$b && $a<$c){
-        if($b<$c ){
+    
+    if($a<=$b && $a<=$c){
+        if($b<=$c ){
             echo "$a,$b,$c";
-        }if($c<$b ){
+        }else{
             echo "$a,$c,$b";
         }
-    }
-    if($b<$a && $b<$c){
-        if($a<$c){
+    }elseif($b<=$a && $b<=$c){
+        if($a<=$c){
             echo "$b,$a,$c";
-        }if($c<$a){
+        }else{
             echo "$b,$c,$a";
         }
-    }
-    if($c<$a && $c<$b){
-        if($a<$b){
+    }elseif($c<=$a && $c<=$b){
+        if($a<=$b){
             echo "$c,$a,$b";
-        }if($b<$a){
+        }else{
             echo "$c,$b,$a";
         }
     }
